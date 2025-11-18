1e5ed105c5f64c25a6b651146ebc6ce56a81a95b
nandini29nd <dhurvenandini29@gmail.com>
2025-11-18 11:58:44 +0530

    first commit

---
+++ Task1.py
@@ -0,0 +1,19 @@
+# Python program to perform basic mathematical operations
+
+# Taking two numbers as input from the user
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Performing operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+# Displaying results
+print("\nResults:")
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+
